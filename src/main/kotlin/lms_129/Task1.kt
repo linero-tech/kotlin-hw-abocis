4482a8693c4a0c9f30cc7f6d6a6a2121ebd09dc4
abocis <90641949+abocis@users.noreply.github.com>
2022-09-27 12:51:46 +0200

    Update task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -2,28 +2,18 @@ package lms_129
 
 fun task1(a: Int, b: Int): Int {
 
-var result = 0
+    var result = 0
 
-    if (a >= b){
+    if (a < b) for (number in a..b)
 
-        result == 0
-
-        println("$result")
-    }
-
-    else
-
-    for (number in a..b ){
         result += number
-
-        println(result)
-    }
+    println("$result")
 
     return result
 
 }
 fun main () {
-    task1( a = 1, b = 5)
+    task1( a = 5, b = 5)
 
 
 }
