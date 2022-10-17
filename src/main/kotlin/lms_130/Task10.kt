53bdcd54eed2ca03d0cce718889e69e5688574d3
abocis <90641949+abocis@users.noreply.github.com>
2022-10-17 02:01:12 +0200

    taskt 10, 12, 13

--- src/main/kotlin/lms_130/Task10.kt
+++ src/main/kotlin/lms_130/Task10.kt
@@ -4,6 +4,24 @@ fun task10(items: List<Int>): List<Int> {
 
     val result = mutableListOf<Int>()
 
+    var number = 0
+
+    for (i in items){
+        if (i  % 2 == 0){
+            number = i * 2
+            result.add(number)
+
+        }else{ i % 2 == 1
+            number = i * 3
+
+            result.add(number)
+
+        }
+    }
+
     return result
 
+}
+fun main(){
+    println(task10(items = listOf(1, 2, 3, 4, 5 )))
 }
--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -1,16 +1,51 @@
 package lms_130
 
+import lms_131.task12
+
 fun task12_1(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val days = listOf<String>("Mondays", "Tuesday", "Wednesdays", "Thursday", "Friday", "Saturday", "Sunday")
+    val maxSellVallue = sells.maxOrNull()
+    val indexOfMaxSellValue = sells.indexOf(maxSellVallue)
+    val result = days[indexOfMaxSellValue]
+
+    println(result)
+
+    return result
 }
 
 fun task12_2(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val days = listOf<String>("Mondays", "Tuesday", "Wednesdays", "Thursday", "Friday", "Saturday", "Sunday")
+    val maxSellVallue = sells.minOrNull()
+    val indexOfMaxSellValue = sells.indexOf(maxSellVallue)
+    val result = days[indexOfMaxSellValue]
+
+    println(result)
+
+    return result
 }
 
 fun task12_3(sells: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    sells.forEach {
+        result += it
+    }
+
+    println(result)
+
+    return result
 }
+
+fun main (){
+    task12_1(sells = listOf(20, 100, 800, 768, 765, 90, 10) )
+    task12_2(sells = listOf(20, 100, 800, 768, 765, 90, 10) )
+    task12_3( sells = listOf(20, 100, 800, 768, 765, 90, 10))
+
+
+
+}
+
+
+
