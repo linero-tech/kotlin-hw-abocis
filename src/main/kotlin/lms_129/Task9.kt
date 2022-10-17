a093550186245d3a0892d7cc2f9ffa799aded0a8
abocis <90641949+abocis@users.noreply.github.com>
2022-10-17 02:07:03 +0200

    update task 9

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -3,8 +3,25 @@ package lms_129
 import java.util.Scanner
 
 fun task9(temperature: String): String {
-  var types = arrayOf("celsius", "fahrenheit")
 
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val magnitude = temperature.substring(0, temperature.length-1).toDouble()
+
+    val isCelsius = temperature.contains("C", ignoreCase = true)
+
+    val result = if (isCelsius){
+      val conversion = (9.0 * magnitude / 5.0) + 32.0
+      "${conversion.toInt()}F"
+    }else {
+      val conversion = (5.0 * (magnitude - 32.0)) / 9.0
+      "${conversion.toInt()}C"
+    }
+
+
+
+  return result
+}
+
+fun main(){
+  println(task9( "50f"))
 }
