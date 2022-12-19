29eee88fc9df590a1e6555319a2da888ae17fc09
abocis <90641949+abocis@users.noreply.github.com>
2022-12-19 11:50:01 +0100

    ..

--- src/main/kotlin/lms_135/Task2.kt
+++ src/main/kotlin/lms_135/Task2.kt
@@ -18,3 +18,18 @@ class Account(var balance: Double) {
         return balance
     }
 }
+fun main () {
+    val seb = Account(100.00)
+
+    seb.deposit(100.00)
+    println(seb.balance)
+
+    seb.deposit(100.00)
+    println(seb.balance)
+
+    seb.withdrawal(500.00)
+    println(seb.balance)
+
+    seb.fee()
+    println(seb.balance)
+}
